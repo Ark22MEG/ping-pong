3fa068822dabf0dc5dcf5b59052c70800b724b20
Ark22MEG <134157091+Ark22MEG@users.noreply.github.com>
2023-06-03 18:15:59 +0200

    Update ping-pong.py
    Добавлен счётчик очков

--- ping-pong.py
+++ ping-pong.py
@@ -48,6 +48,7 @@ game = True
 
 font.init()
 font1 = font.SysFont("Arial", 36)
+font2 = font.SysFont("Arial", 20)
 lose_1 = font1.render('Win player 2', True, (180, 0, 0))
 lose_2 = font1.render('Win player 1', True, (180, 0, 0))
 
@@ -65,11 +66,17 @@ ball_x = 3
 ball_y = 3
 
 while game:
+       
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
         window.fill(back)
+        text_1 = font2.render('Счёт 1 игрока: ' + str(score_1), True, (0,0,0))
+        window.blit(text_1, (10, 10))
+
+        text_2 = font2.render('Счёт 2 игрока: ' + str(score_2), True, (1, 0, 0)) 
+        window.blit(text_2, (440, 10))
         racket_1.update_l()
         racket_2.update_r()
 
@@ -96,15 +103,22 @@ while game:
         if ball.rect.x < 0 :
             window.blit(lose_1,(200, 250))
             finish = True
-        
+            score_2 += 1
         if ball.rect.x > width - 50 :
             window.blit(lose_2,(200, 250))
             finish = True
-
+            score_1 += 1
         racket_1.reset()
         racket_2.reset()
         ball.reset()
     else:
         finish = False
+        time.delay(3000)
+        racket_1 = Player("racket.png",30 ,200 ,4 ,50 , 150)
+        racket_2 = Player("racket.png",520 ,200 ,4 ,50 , 150)
+        ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+        ball_x = 3
+        ball_y = 3
+
     display.update()
     clock.tick(FPS)
